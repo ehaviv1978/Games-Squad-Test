a068e98deeeda44bf9a9700ca900387b2ac255ec
MiriBut <mirixegal@gmail.com>
2021-11-17 22:06:51 +0200

    Firs Commit After Marge Progect

---
+++ Assets/Scripts/ClickOnButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ClickOnButton : MonoBehaviour
+{
+    public string srtingThatHadBeenClicked;
+    public DisplayRedCode redScript;
+    public bool finishChek;
+    [SerializeField] GameObject finishImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+    public void IfClicked()
+    {
+        //string name = gameObject.name.ToString();
+        string name = EventSystem.current.currentSelectedGameObject.name.ToString();
+        char lastLatter = name[name.Length - 1];
+        Debug.Log("strlastLattering lastlatter= "+ lastLatter);
+        CollectClicks(lastLatter.ToString());
+        ChangeRedCode();
+    }
+    void CollectClicks(string clickedString)
+    {
+        srtingThatHadBeenClicked += clickedString;
+        Debug.Log(srtingThatHadBeenClicked);
+    }
+    public void ChangeRedCode()
+    {
+        redScript.ChangeToStars();
+        if (redScript.IsDone())
+            DoneGame();
+    }
+    public bool DoneGame()
+    {
+        Debug.Log("Finito");
+        finishImage.SetActive(true);
+        return true;
+    }
+}
---
+++ Assets/Scripts/DisplayRedCode.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayRedCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int lenghtOfCode;
+    int lenghtForStars;
+    public string _Code;
+    [SerializeField] Text textOfRedCode;
+
+    void Start()
+    {
+        GetRandomCode("12345");
+        lenghtForStars = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void GetRandomCode(string theCode)
+    {
+        //DisplayCodeOnScrean(theCode);
+        lenghtOfCode = theCode.Length;
+        for(int i = 0 ; i<lenghtOfCode ; i++)
+        {
+            _Code = _Code + " " + "_";
+        }
+        textOfRedCode.text = _Code;
+    }
+    string FakeRandomCodeForTest()
+    {
+        return "1234";
+    }
+    public void ChangeToStars()
+    {
+        _Code = "";
+        lenghtForStars++;
+        PrintChars(lenghtForStars,"*");
+        PrintChars(lenghtOfCode - lenghtForStars,"_");
+        textOfRedCode.text = _Code;
+    }
+    public void PrintChars(int lenght, string type)
+    {
+        for (int i = 0; i < lenght; i++)
+        {
+            _Code = _Code + " " + type;
+        }
+    }
+    public bool IsDone()
+    {
+        if (lenghtForStars == lenghtOfCode)
+            return true;
+        else
+            return false;
+    }
+    public void DisplayCodeOnScrean(string codeToDisplay)
+    {
+        foreach (int i in codeToDisplay)
+        Debug.Log(codeToDisplay.Substring(0,i));
+    }
+}
